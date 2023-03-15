a32045ce6ee7dc991864490bbead9f9707c88407
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-03-14 22:55:34 -0600

    Style changes

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+// service-worker.js should be at the root of the project unlike other js files
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 workbox.routing.registerRoute(
     ({request}) => true,

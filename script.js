2f38763b37ef42df84aef0ba937ab10c7d2cae4e
jacobk5391 <118397867+jacobk5391@users.noreply.github.com>
2023-03-30 12:24:21 -0600

    Create script.js and begin javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body"),
+	sidebar = body.querySelector(".sidebar"),
+	toggle = body.querySelector(".toggle"),
+	searchBtn = body.querySelector(".search-box");
+
+toggle.addEventListener("click", () => {
+	sidebar.classList.toggle("close");
+});
